85f4c8fa38beb599cd9d335b4676fac1d1b6c5ba
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-03-28 10:14:17 -0300

    Criação do arquivo do menu lateral

--- DevCursos.InterfaceUsuario/wwwroot/app/home/Home.controller.js
+++ DevCursos.InterfaceUsuario/wwwroot/app/home/Home.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
     "dev/cursos/common/BaseController",
-    'sap/ui/model/json/JSONModel'
-], function (BaseController, JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/core/Fragment',
+	'sap/m/MessageToast',
+	"sap/m/MenuItem"
+], function (BaseController, JSONModel, Fragment, MessageToast, MenuItem) {
     'use strict';
 
     const ROTA_HOME = "home";
@@ -11,7 +14,7 @@ sap.ui.define([
         onInit: function () {
             this.vincularRota(ROTA_HOME, this._aoCoincidirComRotaHome);
         },
-        
+
         _aoCoincidirComRotaHome: function () {
             // let svgLogo = sap.ui.require.toUrl("dev/cursos/resources/images/logo.svg");
             let svgLogo = sap.ui.require.toUrl("dev/cursos/resources/images/puc-goias-horizontal.png");
@@ -21,9 +24,28 @@ sap.ui.define([
                 svgLogo: svgLogo
             }));
         },
-        
+
         aoClicarEmEntar() {
             this.navegarPara("login")
+        },
+
+        onPress: function () {
+            var view = this.getView(),
+                button = view.byId("button");
+
+            if (!this.menuFragment) {
+                this.menuFragment = Fragment.load({
+                    id: view.getId(),
+                    name: "dev.cursos.app.home.fragments.Menu",
+                    controller: this
+                }).then(function (menu) {
+                    menu.openBy(button);
+                    this.menuFragment = menu;
+                    return this.menuFragment;
+                }.bind(this));
+            } else {
+                this.menuFragment.openBy(button);
+            }
         }
     });
 });
